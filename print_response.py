8a8c608563dc3ece898cdd951e66d9356a49a5de
prototyped1 <garywen0@gmail.com>
2025-05-08 20:50:37 +1000

    Initial_test

---
+++ print_response.py
@@ -0,0 +1,11 @@
+from ollama import chat
+from ollama import ChatResponse
+
+response: ChatResponse = chat(model='deepseek-r1:7b', messages=[
+{
+'role': 'user',
+'content': '帮我分析公司的资本支出：2022年公司资本支出为30万元，2023年公司资本支出为35万元',
+},
+])
+
+print(response['message']['content'])
